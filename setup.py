f7a18cfe5b56909465468f0fc180160bfe4efe2b
Konstantine Rybnikov <k-bx@k-bx.com>
2013-01-16 09:35:21 +0700

    add LICENSE and setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='jquery_unparam',
+    version='1.0.0',
+    url='https://bitbucket.org/k_bx/jquery-unparam/',
+    license='BSD',
+    author='Konstantine Rybnikov',
+    author_email='k-bx@k-bx.com',
+    description='A python library to parse jquery.param() string.',
+    long_description=__doc__,
+    py_modules=['jquery_unparam'],
+    platforms='any',
+    install_requires=[],
+)
